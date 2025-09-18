97fdcfcceb74eadd9909450e477bb611f0332338
AtikHasan16 <akhasannaeem@gmail.com>
2025-09-19 01:06:12 +0600

    added country cards and visit button

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -3,12 +3,16 @@ import Country from "./Country/country";
 const Countries = ({ CountriesPromiseData }) => {
   const CountriesData = use(CountriesPromiseData);
   const countries = CountriesData.countries;
-  console.log(countries);
+  //   console.log(countries);
 
   return (
-    <div>
+    <div className="grid grid-cols-3">
       {countries.map((country, index) => (
-        <Country index={index} country={country}></Country>
+        <Country
+          key={country.ccn3.ccn3}
+          index={index}
+          country={country}
+        ></Country>
       ))}
     </div>
   );
--- src/Components/Country/country.jsx
+++ src/Components/Country/country.jsx
@@ -1,14 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
-const country = ({ country, index }) => {
+const Country = ({ country, index }) => {
+  const [visited, setVisited] = useState(false);
+
+  const handleVisit = () => {
+    visited ? setVisited(false) : setVisited(true);
+  };
+
   return (
-    <div className="font-semibold text-2xl">
-      <h2>
-        {" "}
-        {index + 1}. {country.name.common}
-      </h2>
+    <div
+      className={`font-semibold border-2 rounded-2xl m-4 p-4 h-110 ${
+        visited ? "text-cyan-400" : "text-white"
+      }`}
+    >
+      <img className="rounded-2xl mb-6" src={country.flags.flags.png} alt="" />
+      <p>Serial No: {index + 1}</p>
+      <h2>Name: {country.name.common}</h2>
+      <h3>Region: {country.region.region}</h3>
+      <p>Population: {country.population.population}</p>
+      <p className="pb-4">Language: {country.languages.languages.eng}</p>
+      <button onClick={handleVisit}>
+        {visited ? "Visited" : "Not Visited"}{" "}
+      </button>
     </div>
   );
 };
 
-export default country;
+export default Country;
+
+/**
+ * {
+"name": {
+"common": "Jamaica",
+"official": "Jamaica"
+},
+"ccn3": {
+"ccn3": "388"
+},
+"currencies": {
+"currencies": {
+"JMD": {
+"name": "Jamaican dollar",
+"symbol": "$"
+}
+}
+},
+"capital": {
+"capital": [
+"Kingston"
+]
+},
+"region": {
+"region": "Americas"
+},
+"languages": {
+"languages": {
+"eng": "English",
+"jam": "Jamaican Patois"
+}
+},
+"area": {
+"area": 10991
+},
+"cca3": {
+"cca3": "JAM"
+},
+"population": {
+"population": 2961161
+},
+"continents": {
+"continents": [
+"North America"
+]
+},
+"flags": {
+"flags": {
+"png": "https://flagcdn.com/w320/jm.png",
+"svg": "https://flagcdn.com/jm.svg",
+"alt": "The flag of Jamaica is divided by a gold diagonal cross into four alternating triangular areas of green at the top and bottom, and black on the hoist and fly sides"
+}
+}
+},
+ */
